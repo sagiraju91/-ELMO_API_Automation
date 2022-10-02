e8b786e832e7e5d222396d5da52aa6dc5c31c7fd
sowjy <sowjy@DESKTOP-QOIP40E>
2022-10-03 06:11:03 +1100

    SFTP project

---
+++ SFTP/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,35 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SFTP.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+      
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+           
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+  
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+
+        }
+    }
+}
